1a2cdb6beba176630f26cc8ef5cecd07f580de52
Jordan Bragon <jordan.bragon@aluno.ufop.edu.br>
2018-12-27 21:40:10 -0200

    M1#A08

--- modulo1/src/app.js
+++ modulo1/src/app.js
@@ -6,8 +6,10 @@ import Title from './title';
 const App = React.createClass({
     render: function(){
         return (
-            <div>
+            <div className='container'>
                 <Title name='Jordan Bragon'/>
+                <label htmlFor='input'>Input</label>
+                <input type='text' id='input' />
             </div>
         )
     }
