d99874b0ef74f0138d2204b7de440320ec3d2327
Jordan Bragon <jordan.bragon@aluno.ufop.edu.br>
2018-12-29 11:39:33 -0200

    M1#A13

--- modulo1/src/app.js
+++ modulo1/src/app.js
@@ -1,13 +1,15 @@
 'use strict'
 
-import React, {Component} from 'react'
-import Title from './title';
+import React, { Component } from 'react'
+import Square from './square'
 
 class App extends Component {
     render () {
         return (
             <div className='container'>
-                <Title name='Jordan' lastname={'Bragon'}/>
+                {['blue', 'red', 'green'].map((square) => (
+                    <Square key={square} color={square} />
+                ))}
             </div>
         )
     }
