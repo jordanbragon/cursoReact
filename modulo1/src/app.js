3c037984d34ebc4a974e1fe7821ab08b8f83c82b
Jordan Bragon <jordan.bragon@aluno.ufop.edu.br>
2019-01-14 21:44:31 -0200

    M1#A28

--- modulo1/src/app.js
+++ modulo1/src/app.js
@@ -1,20 +1,30 @@
 'use strict'
 
 import React, { Component } from 'react'
-import Button from './button'
 
-class App extends Component {
-   
+class App extends Component{
+
+    constructor () {
+        super()
+        this.state = {
+            value: 'valor inicial'
+        }
+    }
     render () {
-        console.log('render app')
         return (
             <div>
-                <Button handleClick={() => console.log('clicou')}>
-                    Clique em mim
-                </Button>
+                <form>
+                    <input type='text' value={this.state.value} onChange={(e) => {
+                        this.setState({
+                            value: e.target.value
+                        })
+                    }}/>
+                </form>
             </div>
         )
     }
 }
+   
+
 
 export default App
